70f33c51f9879d2c30224ed46295d472c8a30a01
ddyln <ddlan99tn@gmail.com>
2024-10-23 12:45:55 +0700

    add algorithm selector, restart button

--- visualize.py
+++ visualize.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 import time as TIME
+from tkinter import ttk
+from a_star import a_star
+from ucs import ucs
+from dfs import dfs
 
 sz = 64
 start_x, start_y = 0, 0
@@ -42,22 +46,19 @@ def readMap(matrix, file_name):
             elif matrix[i][j] == '+': # ares + switches
                 matrix[i][j] = 6
                 player_pos = [i, j]
-    return player_pos, stones_pos, switches_pos, walls_pos
+    return w, h, player_pos, stones_pos, switches_pos, walls_pos
 
+actionsMap = 'urdlURDL'
 file_name = 'input-01.txt'
-matrix = [[]]
-player_pos, stones_pos, switches_pos, walls_pos = readMap(matrix, file_name)
-actions = 'rruLulDrdLLUluRRRRRRRRurDD'
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-actionsMap = 'urdlURDL'
 
 class CanvasDemo:
     def __init__(self, root):
         self.root = root
-        self.width = 64 * 14
+        self.width = 64 * 15
         self.height = 64 * 10
-        self.canvas = tk.Canvas(root, width=self.width, height=self.height, bg='white')
+        self.canvas = tk.Canvas(root, width=self.width, height=self.height, bg='gray')
         self.root.resizable(False, False)
         self.canvas.pack()
         self.root.title("Search Visualization")
@@ -66,20 +67,9 @@ class CanvasDemo:
         self.crate_image = tk.PhotoImage(file="./Assets/crate.png")
         self.goal_image = tk.PhotoImage(file="./Assets/goal.png")
         self.block_image = tk.PhotoImage(file="./Assets/block.png")
-        for i in range(self.height // sz):
-            for j in range(self.width // sz):
-                self.root.after(10, lambda pos=(i,j): self.drawCell(pos))
-        # print(player_pos)
-        self.root.after(20, lambda: self.drawCell(player_pos, self.player_image))
-        for i in stones_pos:
-            self.root.after(20, lambda pos=i: self.drawCell(pos, self.crate_image))
-        for i in switches_pos:
-            self.root.after(20, lambda pos=i: self.drawCell(pos, self.goal_image))
-        for i in walls_pos:
-            self.root.after(20, lambda pos=i: self.drawCell(pos, self.block_image))
-        self.running = False
-        self.player_pos = player_pos 
-        self.animation_state = 0
+        self.switches_pos = ()
+        self.speed = 500
+        # self.restart()
         self.button = tk.Button(
             root, 
             text="Start", 
@@ -87,7 +77,8 @@ class CanvasDemo:
             width=8,
             height=2
         )
-        self.button.place(x = sz * 13, y = sz * 5)
+        self.button.place(x = sz * 13, y = sz * 2)
+        
         self.button = tk.Button(
             root, 
             text="Stop", 
@@ -95,29 +86,91 @@ class CanvasDemo:
             width=8,
             height=2
         )
-        self.button.place(x = sz * 13, y = sz * 6)
+        self.button.place(x = sz * 13, y = sz * 3.25)
+
+        self.button = tk.Button(
+            root, 
+            text="Restart", 
+            command=self.restart,
+            width=8,
+            height=2
+        )
+        self.button.place(x = sz * 13, y = sz * 4.5)
+
+        self.algorithm_label = tk.Label(root, 
+            text="Select Algorithm",
+            font=("Arial", 13)
+        )
+        self.algorithm_label.place(x=sz * 12.5, y=sz * 0.5)
+        self.algorithm_combobox = ttk.Combobox(root, 
+            values=["DFS", "UCS", "A*"], 
+            state='readonly',
+            width=10,
+            font=("Arial", 12)
+        )
+        self.actions = ''
+        self.algorithm_combobox.set("DFS")  # Set default value
+        self.algorithm_combobox.place(x=sz * 12 + sz * 3 // 4, y=sz * 1)
+        self.algorithm_combobox.bind("<<ComboboxSelected>>", self.on_algorithm_selected)
+        self.on_algorithm_selected(tk.Event())
+    def restart(self):
+        matrix = [[]]
+        W, H, player_pos, stones_pos,self.switches_pos, walls_pos = readMap(matrix, file_name)
+
+        for i in range(H):
+            for j in range(W):
+                self.root.after(10, lambda pos=(i,j): self.drawCell(pos))
+        self.root.after(20, lambda: self.drawCell(player_pos, self.player_image))
+        for i in stones_pos:
+            self.root.after(20, lambda pos=i: self.drawCell(pos, self.crate_image))
+        for i in self.switches_pos:
+            self.root.after(20, lambda pos=i: self.drawCell(pos, self.goal_image))
+        for i in walls_pos:
+            self.root.after(20, lambda pos=i: self.drawCell(pos, self.block_image))
+        self.running = False
+        self.player_pos = player_pos 
+        self.animation_state = 0
+
+    def on_algorithm_selected(self, event):
+        selected_algorithm = self.algorithm_combobox.get()
+        if selected_algorithm == 'DFS':
+            self.root.after(0, self.run_dfs)
+        elif selected_algorithm == 'UCS':
+            self.root.after(0, self.run_ucs)
+        elif selected_algorithm == 'A*':
+            self.root.after(0, self.run_a_star)
+        self.restart()
 
     def start(self):
         if not self.running:
             self.running = True
-            self.animate()
+            self.animate() 
+        
+    def run_dfs(self):
+        self.actions, steps, weight, node, time, memory = dfs(file_name)
+        
+    def run_ucs(self):
+        self.actions, steps, weight, node, time, memory = ucs(file_name)
+
+    def run_a_star(self):
+        self.actions, steps, weight, node, time, memory = a_star(file_name)
 
     def stop(self):
         self.running = False
 
     def animate(self):
         if self.running:
-            if self.animation_state < len(actions):
-                ok = False
-                for i in switches_pos:
-                    if self.player_pos == [i[0], i[1]]:
-                        ok = True
+            if self.animation_state < len(self.actions):
+                # ok = False
+                # for i in self.switches_pos:
+                #     if self.player_pos == [i[0], i[1]]:
+                #         ok = True
                 self.drawCell(self.player_pos)
-                if ok: self.drawCell(self.player_pos, self.goal_image)
-                self.player_pos = self.move(self.player_pos, actionsMap.find(actions[self.animation_state]))
+                # if ok: self.drawCell(self.player_pos, self.goal_image)
+                self.player_pos = self.move(self.player_pos, actionsMap.find(self.actions[self.animation_state]))
                 self.drawCell(self.player_pos, self.player_image)
                 self.animation_state += 1
-                self.root.after(1000, self.animate)
+                self.root.after(self.speed, self.animate)
             else:
                 self.running = False
 
@@ -143,6 +196,8 @@ class CanvasDemo:
             self.drawCell(pos)
             newpos = [pos[0] + dx[direction], pos[1] + dy[direction]]
             self.drawCell(newpos, self.crate_image)
+        for i in self.switches_pos:
+            self.drawCell(i, self.goal_image)
         return pos
 
 if __name__ == "__main__":
