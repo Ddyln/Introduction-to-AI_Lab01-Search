dc20d4c54cfc768a04402b660af62881582ff80c
Dang Duy Lan <ddlan99tn@gmail.com>
2024-10-26 00:17:51 +0700

    change argument for main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def runAll():
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         assert len(sys.argv) == 2, "Invalid number of arguments"
-        assert sys.argv[1] in ['-y', '-n', '-Y', '-N'], "Invalid arguments"
-        visualize() if sys.argv[1] in ['-y', '-Y'] else runAllCase()
+        assert sys.argv[1] == '-v', "Invalid arguments"
+        visualize()
     else:
         runAll()
